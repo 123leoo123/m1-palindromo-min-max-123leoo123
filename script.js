b684c60f441241b05f6f24a63830eafbce7a6a44
Leonardo Leao <leonardodleao@gmail.com>
2024-04-06 01:33:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(palindromo){
+   const palavraTratada = str.toLowerCase();
+    
+    for(let i = 0; i <str.length; i++){
+        for(let j = -1; str.length; i--){
+          if(str[i].length == str.length[j]){
+            return true
+          }   
+        }
+       
+    }
 }
 
 function arrayMaxMin(arr){
